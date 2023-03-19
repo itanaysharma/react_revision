9478332076618e578eb523b60680b877afb91dd0
itanaysharma <thisistanaysharma@gmail.com>
2023-03-19 23:19:39 +0530

    used object destructuring of JS to pass the props properly

--- src/App.js
+++ src/App.js
@@ -53,15 +53,16 @@ const Search = (props) => (
     />
   </div>
 );
-const List = (props) =>
-  props.list.map((item) => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+const List = ({ list }) =>
+  list.map((item) => <Item key={item.objectID} item={item} />);
+const Item = ({ item }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+);
 export default App;
