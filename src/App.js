0e10ade639fd279b5dac41948d9244c0ba56f106
itanaysharma <thisistanaysharma@gmail.com>
2023-03-20 17:36:05 +0530

    autofocus using imperative method means telling react  how to do

--- src/App.js
+++ src/App.js
@@ -68,19 +68,29 @@ const InputWithLabel = ({
   onInputChange,
   isFocused,
   children,
-}) => (
-  <>
-    <label htmlFor={id}>{children} </label>
-    &nbsp;
-    <input
-      id={id}
-      type={type}
-      value={value}
-      autoFocus={isFocused}
-      onChange={onInputChange}
-    />
-  </>
-);
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
 const List = ({ list }) =>
   list.map((item) => <Item key={item.objectID} item={item} />);
 const Item = ({ item }) => (
