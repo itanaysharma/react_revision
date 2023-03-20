a66837d928d5c979a6df5fa2d6b5e38e5c082042
itanaysharma <thisistanaysharma@gmail.com>
2023-03-20 17:21:12 +0530

    React component composition
    Removed lable variable from compnent instead used a child component

--- src/App.js
+++ src/App.js
@@ -47,10 +47,11 @@ const App = () => {
 
       <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm}
         onInputChange={handleSearch}
-      />
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
@@ -59,9 +60,15 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => (
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  children,
+}) => (
   <>
-    <label htmlFor={id}>{label} </label>
+    <label htmlFor={id}>{children} </label>
     &nbsp;
     <input id={id} type={type} value={value} onChange={onInputChange} />
   </>
