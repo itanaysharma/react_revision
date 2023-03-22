afcdb2d89322f3942ffa65fdfa0a33902c124d54
itanaysharma <thisistanaysharma@gmail.com>
2023-03-22 22:47:42 +0530

    inline handler in JSX

--- src/App.js
+++ src/App.js
@@ -12,29 +12,36 @@ const useSemiPersistentState = (key, initialSate) => {
   return [value, setValue];
 };
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+  const [stories, setStories] = React.useState(initialStories);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+  };
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
   };
 
   const searchedStories = stories.filter((story) =>
@@ -56,7 +63,7 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
@@ -91,9 +98,11 @@ const InputWithLabel = ({
     </>
   );
 };
-const List = ({ list }) =>
-  list.map((item) => <Item key={item.objectID} item={item} />);
-const Item = ({ item }) => (
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -101,6 +110,10 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}></button>
+      {/* the onClick is looking like this because we have to pass item argument in callback which is only possible by this or bind method */}
+    </span>
   </div>
 );
 export default App;
