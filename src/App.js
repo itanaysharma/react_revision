552f958bfc397cabce1f420a996f7c5b1f1bc54d
itanaysharma <thisistanaysharma@gmail.com>
2023-04-07 22:23:39 +0530

    Using CSS in React

--- src/App.js
+++ src/App.js
@@ -89,15 +89,14 @@ const App = () => {
     event.preventDefault();
   };
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      <hr />
 
       {stories.isError && <p>Something went wrong ...</p>}
 
@@ -110,7 +109,7 @@ const App = () => {
   );
 };
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -120,7 +119,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -143,7 +146,9 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
       &nbsp;
       <input
         ref={inputRef}
@@ -151,6 +156,7 @@ const InputWithLabel = ({
         type={type}
         value={value}
         onChange={onInputChange}
+        className="input"
       />
     </>
   );
@@ -160,7 +166,7 @@ const List = ({ list, onRemoveItem }) =>
     <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
   ));
 const Item = ({ item, onRemoveItem }) => (
-  <div>
+  <div className="item">
     <span>
       <a href={item.url}>{item.title}</a>
     </span>
@@ -168,7 +174,17 @@ const Item = ({ item, onRemoveItem }) => (
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
     <span>
-      <button type="button" onClick={() => onRemoveItem(item)}></button>
+      {/* <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}> */}
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className="button button_small"
+      >
+        Dismiss
+      </button>
       {/* the onClick is looking like this because we have to pass item argument in callback which is only possible by this or bind method */}
     </span>
   </div>
